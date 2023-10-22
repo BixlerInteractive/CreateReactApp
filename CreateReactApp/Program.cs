63aa94f81b5c1071fe52551bfd7ffb56ff8b6e1d
Will Bixler <will@bixlerinteractive.com>
2023-10-22 16:10:01 -0600

    Added viewport size to index.html

--- CreateReactApp/Program.cs
+++ CreateReactApp/Program.cs
@@ -82,6 +82,7 @@ async Task CreateFiles()
                                <html lang="en">
                                <head>
                                    <meta charset="UTF-8">
+                                   <meta content="width=device-width, initial-scale=1.0" name="viewport">
                                    <title>{appName}</title>
                                </head>
                                <body>
